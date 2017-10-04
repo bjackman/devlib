7516448f1962104d5d58f9683e0ad16de26eb247
Brendan Jackman <brendan.jackman@arm.com>
2017-10-04 17:06:22 +0100

    WIP

--- devlib/instrument/acmecape.py
+++ devlib/instrument/acmecape.py
@@ -39,12 +39,12 @@ class AcmeCapeInstrument(Instrument):
     def __init__(self, target,
                  iio_capture=which('iio-capture'),
                  host='baylibre-acme.local',
-                 iio_device='iio:device0',
+                 iio_devices=['iio:device0'],
                  buffer_size=256):
         super(AcmeCapeInstrument, self).__init__(target)
         self.iio_capture = iio_capture
         self.host = host
-        self.iio_device = iio_device
+        self.iio_devices = iio_devices
         self.buffer_size = buffer_size
         self.sample_rate_hz = 100
         if self.iio_capture is None:
@@ -52,80 +52,120 @@ class AcmeCapeInstrument(Instrument):
         self.command = None
         self.process = None
 
-        self.add_channel('shunt', 'voltage')
-        self.add_channel('bus', 'voltage')
-        self.add_channel('device', 'power')
-        self.add_channel('device', 'current')
-        self.add_channel('timestamp', 'time_ms')
+        for device in self.iio_devices:
+            self.add_channel('shunt_{}'.format(device), 'voltage',
+                             iio_device=device, iio_column='vshunt mV')
+            self.add_channel('bus_{}'.format(device), 'voltage',
+                             iio_device=device, iio_column='vbus mV')
+            self.add_channel('device_{}'.format(device), 'power',
+                             iio_device=device, iio_column='power mW')
+            self.add_channel('device_{}'.format(device), 'current',
+                             iio_device=device, iio_column='current mA')
+            self.add_channel('timestamp_{}'.format(device), 'time_ms',
+                             iio_device=device, iio_column='timestamp ms')
 
     def reset(self, sites=None, kinds=None, channels=None):
         super(AcmeCapeInstrument, self).reset(sites, kinds, channels)
-        self.raw_data_file = tempfile.mkstemp('.csv')[1]
-        params = dict(
-            iio_capture=self.iio_capture,
-            host=self.host,
-            buffer_size=self.buffer_size,
-            iio_device=self.iio_device,
-            outfile=self.raw_data_file
-        )
-        self.command = IIOCAP_CMD_TEMPLATE.substitute(**params)
-        self.logger.debug('ACME cape command: {}'.format(self.command))
+
+        self.commands = []
+        self.raw_data_files = []
+        for device in self.iio_devices:
+            raw_data_file = tempfile.mkstemp('{}.csv'.format(device))[1]
+            params = dict(
+                iio_capture=self.iio_capture,
+                host=self.host,
+                buffer_size=self.buffer_size,
+                iio_device=device,
+                outfile=raw_data_file
+            )
+            self.raw_data_files.append(raw_data_file)
+            self.commands.append(IIOCAP_CMD_TEMPLATE.substitute(**params))
+            self.logger.debug('ACME cape command: {}'.format(self.command))
 
     def start(self):
-        self.process = Popen(self.command.split(), stdout=PIPE, stderr=STDOUT)
+        self.processes = []
+        for command in self.commands:
+            self.processes.append(Popen(command.split(), stdout=PIPE, stderr=STDOUT))
 
     def stop(self):
-        self.process.terminate()
-        timeout_secs = 10
-        output = ''
-        for _ in xrange(timeout_secs):
-            if self.process.poll() is not None:
-                break
-            time.sleep(1)
-        else:
-            output += _read_nonblock(self.process.stdout)
-            self.process.kill()
-            self.logger.error('iio-capture did not terminate gracefully')
-            if self.process.poll() is None:
-                msg = 'Could not terminate iio-capture:\n{}'
-                raise HostError(msg.format(output))
-        if self.process.returncode != 15: # iio-capture exits with 15 when killed
-            output += self.process.stdout.read()
-            raise HostError('iio-capture exited with an error ({}), output:\n{}'
-                            .format(self.process.returncode, output))
-        if not os.path.isfile(self.raw_data_file):
-            raise HostError('Output CSV not generated.')
+        for process in self.processes:
+            self.process.terminate()
+            timeout_secs = 10
+            output = ''
+            for _ in xrange(timeout_secs):
+                if self.process.poll() is not None:
+                    break
+                time.sleep(1)
+            else:
+                output += _read_nonblock(self.process.stdout)
+                self.process.kill()
+                self.logger.error('iio-capture did not terminate gracefully')
+                if self.process.poll() is None:
+                    msg = 'Could not terminate iio-capture:\n{}'
+                    raise HostError(msg.format(output))
+            if self.process.returncode != 15: # iio-capture exits with 15 when killed
+                output += self.process.stdout.read()
+                raise HostError('iio-capture exited with an error ({}), output:\n{}'
+                                .format(self.process.returncode, output))
+            if not os.path.isfile(self.raw_data_file):
+                raise HostError('Output CSV not generated.')
 
     def get_data(self, outfile):
-        if os.stat(self.raw_data_file).st_size == 0:
-            self.logger.warning('"{}" appears to be empty'.format(self.raw_data_file))
-            return
+        class DeviceReader(object):
+            def __init__(self, raw_data_file, columns):
+                self._reader = csv.DictReader(open(raw_data_file, 'rb'),
+                                              skipinitialspace=Truee)
+                self._current_row = self._reader.next()
+                self.columns = columns
+                self.finished = False
 
-        all_channels = [c.label for c in self.list_channels()]
-        active_channels = [c.label for c in self.active_channels]
-        active_indexes = [all_channels.index(ac) for ac in active_channels]
+            @property
+            def timestamp(self):
+                return self._current_row['timestamp ms']
 
-        with open(self.raw_data_file, 'rb') as fh:
-            with open(outfile, 'wb') as wfh:
-                writer = csv.writer(wfh)
-                writer.writerow(active_channels)
+            @property
+            def current_row(self):
+                return [self._current_row[c] for c in self.columns]
 
-                reader = csv.reader(fh, skipinitialspace=True)
-                header = reader.next()
-                ts_index = header.index('timestamp ms')
+            def pop_row(self):
+                try:
+                    self._current_row = self._reader.next()
+                except StopIteration:
+                    self.finished = True
 
+        active_devices = set(c.iio_device for c in self.active_channels)
+        readers = {}
+        for device, raw_data_file in zip(self.iio_devices, self.raw_data_files):
+            if device not in self.active_devices:
+                continue
 
-                for row in reader:
-                    output_row = []
-                    for i in active_indexes:
-                        if i == ts_index:
-                            # Leave time in ms
-                            output_row.append(float(row[i]))
-                        else:
-                            # Convert rest into standard units.
-                            output_row.append(float(row[i])/1000)
-                    writer.writerow(output_row)
+            if os.stat(raw_data_file).st_size == 0:
+                self.logger.warning('"{}" appears to be empty'.format(self.raw_data_file))
+                continue
+
+            columns = [c.iio_column for c in self.active_channels
+                       if c.iio_device == device]
+            readers[device] = DeviceReader(raw_data_file, columns)
+
+        with open(outfile, 'wb') as wfh:
+            writer = csv.writer(wfh)
+            # Write column headers
+            writer.writerow([c.label for c in self.active_channels])
+
+            while active_devices:
+                row = []
+                for channel in self.active_channels:
+                    reader = readers[channel.iio_device]
+                    row.append(reader.current_row[channel.iio_column])
+
+                writer.writerow(row)
+
+                device_to_pop = min(active_devices, key=lambda d: readers[d].timestamp)
+                reader_to_pop = readers[device_to_pop]
+                if reader_to_pop.finished:
+                    active_devices.remove(reader_to_pop)
+
         return MeasurementsCsv(outfile, self.active_channels, self.sample_rate_hz)
 
     def get_raw(self):
-        return [self.raw_data_file]
+        return self.raw_data_files
